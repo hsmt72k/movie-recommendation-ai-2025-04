85ad37c610c6eedeb1ebad04f6ae2a9ce74990f7
hsmt72k <hsmt72k@gmail.com>
2025-04-07 15:49:41 +0900

    02. Setup shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h1 className="font-extrabold">
-        ベクトル埋め込みを使用したおすすめ映画アプリ
-      </h1>
+      <div className="flex flex-col items-center gap-6">
+        <h1 className="font-extrabold">
+          ベクトル埋め込みを使用したおすすめ映画アプリ
+        </h1>
+        <Button
+          size="xl"
+          variant="destructive"
+          className="text-6xl font-host-grotesk hover:cursor-pointer hover:opacity-60"
+        >
+          Click me!
+        </Button>
+      </div>
     </div>
   );
 }
